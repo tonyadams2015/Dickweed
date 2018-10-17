352a12f16bf18fbc91556cd457fa66286ad673ae
Tony Adams <tonysbrewery@gmail.com>
2018-10-17 19:11:31 +1300

    Clean up code left over from the old encoder switch

--- v_main.ino
+++ v_main.ino
@@ -5,15 +5,12 @@ SimpleTimer timer;
 void setup (void)
 {
   enc_init ();
-  //enc_sw_init ();
   bb_init ();
   lcd_init ();
   therm_init ();
   sm_init ();
   timer.setInterval(1000, therm_update);
   timer.setInterval(100, enc_update);
-  //timer.setInterval(100, enc_sw_update);
-
   therm_update ();
 }
 
